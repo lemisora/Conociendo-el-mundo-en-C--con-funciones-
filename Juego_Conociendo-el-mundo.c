a2981f7596c6950f7f4d79a5386a915b2561f9e9
lemisora <leonardo.michel.dom@gmail.com>
2022-11-09 19:23:19 -0600

    Función que pide nombres Se ha añadido haciendo referencia a un apuntador que está en main

--- Juego_Conociendo-el-mundo.c
+++ Juego_Conociendo-el-mundo.c
@@ -4,14 +4,22 @@
 #include <time.h>
 #define max 50
 
-struct registroNom
+struct registroNom/*Registro que sirve para almacenar el nombre de cada usuario*/
 {
     char nombre[max];
 };
 
+void pideNombres(struct registroNom *a)
+{
+    printf("Ingrese su nombre el primer jugador:\n");
+    scanf("%s",a[0].nombre);
+    printf("Ingrese su nombre el segundo jugador:\n");
+    scanf("%s",a[1].nombre);
+}
+
 int main()
 {
-    int a=0;
-
+    struct registroNom p;
+    pideNombres(&p);
     return 0;
 }
