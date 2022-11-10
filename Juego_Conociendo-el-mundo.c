6a647f789620f9849f29321f3f7633faf65323ee
lemisora <leonardo.michel.dom@gmail.com>
2022-11-09 23:50:38 -0600

    Se añadieron los datos que usarán para comparar
    Listas de:
    - Ciudades
    - Capitales
    - Idiomas
    
    Matriz bidimensional para almacenar los que soliciten los jugadores

--- Juego_Conociendo-el-mundo.c
+++ Juego_Conociendo-el-mundo.c
@@ -9,18 +9,36 @@ struct registroNom/*Registro que sirve para almacenar el nombre de cada usuario*
     char nombre[max];
 };
 
-void pideNombres(struct registroNom *a)
+void pideNombres(struct registroNom *a)/*Función que solicita los nombres*/
 {
-    printf("Ingrese su nombre el primer jugador:\n");
+    printf("Primer jugador ingrese su nombre:\n");
     scanf("%s",a[0].nombre);
-    printf("Hola,esto es una prueba\n");
-    printf("Ingrese su nombre el segundo jugador:\n");
+    printf("Segundo jugador ingrese su nombre:\n");
     scanf("%s",a[1].nombre);
 }
 
 int main()
 {
-    struct registroNom p;
-    pideNombres(&p);
+    int i,num=0;
+    printf("Ingrese el número de países con los que quiere jugar:\n");
+    scanf("%d",&num);
+    char paises[][max]={"Irán","China","India","México","México","Francia","Irán","Francia","Tanzania"},
+        ciudades[][max]={"Ardebil","Cantón","Chennai","Tasquillo","Puebla","Niza","Hamadán","Normandía","Iringa"},
+        capitales[][max]={"Teherán","Pekín","Nueva Dehli","Ciudad de México","Ciudad de México","París","Teherán","París","Dodoma"},
+        idiomas[][max]={"Persa","Cantonés","Tamilés","Otomí","Español","Francés","Persa","Normando","Bantú"};
+
+    char tabla[3][num][max];
+
+    struct registroNom nom;
+    pideNombres(&nom);
+    for (i=0;i<num;i++)
+    {
+        strcpy(tabla[0][i],ciudades[i]);
+        strcpy(tabla[1][i],capitales[i]);
+        strcpy(tabla[2][i],idiomas[i]);
+    }
+
+    
+    
     return 0;
 }
