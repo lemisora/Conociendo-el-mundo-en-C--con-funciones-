7136de03e8f4fc5a992b4a1ece9b8e7d67b5cff6
lemisora <leonardo.michel.dom@gmail.com>
2022-11-10 00:33:07 -0600

    Funciones añadidas - Aleatorio - muestraPregunta +Se solicita memoria para los registros+  +Se muestra quien tiene el turno después de ingresar nombres+

--- Juego_Conociendo-el-mundo.c
+++ Juego_Conociendo-el-mundo.c
@@ -9,6 +9,14 @@ struct registroNom/*Registro que sirve para almacenar el nombre de cada usuario*
     char nombre[max];
 };
 
+int aleatorio(int desde, int hasta)/*Función que otorga un número aleatorio dándole dos números que son el rango que abarca*/
+{
+    int a;
+    srand(time(NULL));
+    a=rand()%hasta+desde;
+    return a;
+}
+
 void pideNombres(struct registroNom *a)/*Función que solicita los nombres*/
 {
     printf("Primer jugador ingrese su nombre:\n");
@@ -17,9 +25,14 @@ void pideNombres(struct registroNom *a)/*Función que solicita los nombres*/
     scanf("%s",a[1].nombre);
 }
 
+void muestraPregunta()
+{
+
+}
+
 int main()
 {
-    int i,num=0;
+    int i,turno=0,n=2,num=0;
     printf("Ingrese el número de países con los que quiere jugar:\n");
     scanf("%d",&num);
     char paises[][max]={"Irán","China","India","México","México","Francia","Irán","Francia","Tanzania"},
@@ -29,8 +42,9 @@ int main()
 
     char tabla[3][num][max];
 
-    struct registroNom nom;
-    pideNombres(&nom);
+    struct registroNom *nom;
+    nom=(struct registroNom*)malloc(n*sizeof(struct registroNom));/*Se solicita espacio en memoria al compilador y al OS para los registros de nombres*/
+    pideNombres(nom);
     for (i=0;i<num;i++)
     {
         strcpy(tabla[0][i],ciudades[i]);
@@ -38,7 +52,11 @@ int main()
         strcpy(tabla[2][i],idiomas[i]);
     }
 
-    
+    turno=aleatorio(2,1);
+    if (turno==1)
+        printf("Empieza el jugador %s\n",nom[0].nombre);
+    else
+        printf("Empieza el jugador %s\n",nom[1].nombre);
     
     return 0;
 }
